102d6fd13fa7ab81de1805344b57875fe1a46dc0
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-09-25 18:35:16 +0600

    Add files via upload

---
+++ string copy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   char str1[20]="HELLO BRO";
+   char str2[20];
+   int i=0;
+   while(str1[i]!='\0')
+   {
+       str2[i]=str1[i];
+       i++;
+   }
+   str2[i]='\0';
+
+   printf("%s",str2);
+
+
+
+
+    return 0;
+}
